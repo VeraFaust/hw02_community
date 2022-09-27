03a284b280d16bea4b3b5335b6d3fb2fbe443eb9
Вера Фауст <faust.vera@yandex.ru>
2022-09-27 21:27:18 +0300

    third project

---
+++ yatube/posts/urls.py
@@ -0,0 +1,13 @@
+# posts/urls.py
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+  # Главная страница
+    path('', views.index, name='index'),
+  # Страница сообществ
+    path('group/<slug:slug>/', views.group_posts, name='group_list')
+]
