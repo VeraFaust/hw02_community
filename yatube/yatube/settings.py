afee4b906ee2d5672a1886d85194993c62ef4934
VeraFaust <110284629+VeraFaust@users.noreply.github.com>
2023-11-27 13:44:21 +0300

    Update settings.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = bool(os.getenv('DEBUG', 'False').lower())
+DEBUG = bool(os.getenv('DEBUG', 'True').lower())
 
 ALLOWED_HOSTS = []
 
